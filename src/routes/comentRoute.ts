fb6e39deea365a18794d8bb35273541775935ce7
Arthursouto099 <arthurtavaressouto@gmail.com>
2025-01-08 15:31:30 -0300

    update id_user from comentario

--- src/routes/comentRoute.ts
+++ src/routes/comentRoute.ts
@@ -45,11 +45,12 @@ comentRouter.post('/insertComent', async (req: Request<Coment>, res: Response): 
 
 })
 
-comentRouter.put('/updateComent/:id',  async (req: Request<Coment>, res: Response): Promise<any> => {
+comentRouter.put('/updateComent/:id',  async (req: Request, res: Response): Promise<any> => {
     try {
         const {comentario, id_post, id_user, user_name} = req.body
+        const id = req.params.id
         const coment = new Coment(comentario, id_post, id_user, user_name)
-        await ComentController.PutComent(Number(req.params), coment)
+        await ComentController.PutComent(Number(id), coment)
         return res.status(201).json({message: 'Comentario editado com sucesso'})
     }
     catch(error: any) {
