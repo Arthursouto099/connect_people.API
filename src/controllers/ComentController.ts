8f143a9ada5b96b82d82e70aad2f6e2735ac5b32
Arthursouto099 <arthurtavaressouto@gmail.com>
2025-01-08 15:19:46 -0300

    update id_user from comentario

--- src/controllers/ComentController.ts
+++ src/controllers/ComentController.ts
@@ -18,6 +18,9 @@ export class ComentController {
         const result = await sql`insert into comentario (comentario, id_post, id_user, user_name) values (${[coment.comentario]}, ${coment.id_post}, ${coment.id_user}, ${coment.user_name} )`
     }
     
+    static async PutComent(id: number, coment: Coment): Promise<void> {
+        const result = await sql`update comentario set comentario = ${coment.comentario}, id_post = ${coment.id_post}, id_user = ${coment.id_user}, user_name = ${coment.user_name} where id = ${id} `
+    }
 
     static async deleteComent(id: number): Promise<void> {
         const result = await sql`delete from comentario where id = ${id}`
--- src/routes/comentRoute.ts
+++ src/routes/comentRoute.ts
@@ -45,6 +45,17 @@ comentRouter.post('/insertComent', async (req: Request<Coment>, res: Response): 
 
 })
 
+comentRouter.put('/updateComent/:id',  async (req: Request<Coment>, res: Response): Promise<any> => {
+    try {
+        const {comentario, id_post, id_user, user_name} = req.body
+        const coment = new Coment(comentario, id_post, id_user, user_name)
+        await ComentController.PutComent(Number(req.params), coment)
+        return res.status(201).json({message: 'Comentario editado com sucesso'})
+    }
+    catch(error: any) {
+        return res.status(500).json({error})
+    }
+} )
 
 comentRouter.delete('/deleteComent/:id', async (req: Request, res: Response): Promise<any> => {
 
